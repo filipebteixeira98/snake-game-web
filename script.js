d9faed146762b00ab89590d7b949a70ed26c000a
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2022-05-27 07:15:14 -0300

    feat: created key event listener to add snake movement

--- script.js
+++ script.js
@@ -24,6 +24,15 @@ function createSnake() {
   }
 }
 
+document.addEventListener('keydown', createSnakeMovement);
+
+function createSnakeMovement(event) {
+  if (event.keyCode == 37 && direction != 'right') direction = 'left';
+  if (event.keyCode == 38 && direction != 'down') direction = 'up';
+  if (event.keyCode == 39 && direction != 'left') direction = 'right';
+  if (event.keyCode == 40 && direction != 'up') direction = 'down';
+}
+
 function startGame() {
   createBackground();
 
